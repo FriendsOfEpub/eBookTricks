c93bb07aee14220ed707225ab44d0ee9dd9601d2
JayPanoz <panozpublishingteam@gmail.com>
2017-01-03 14:11:11 +0100

    init web app

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# eBookTricks
-A collection of CSS snippets to help you do progressive enhancement in eBooks
+# Blitz eBook Tricks
+A collection of CSS snippets to do progressive enhancement and achieve better typography, layout and UX in eBooks.
+
+## How-to
+
+### Use
+
+**If you’re using a mouse/touch:**
+
+- click the “Expand/Collapse button” to toggle all details
+- click “Details” to display more information
+
+**If you’re using a keyboard:**
+
+- press “tab” to navigate menu, details and links
+- press “enter” or “space” to display details (including toggle button)
+
+### Install
+
+If you’re on iOS/Android/Windows 8–10, you can actually install this web app. Just visit the page and “Add to homescreen”.
+
+Thanks to service workers, it will even work offline on Android, Chrome, Opera or Firefox (appcache fallback for iOS).
+
+## Summary
+
+The Blitz eBook Tricks collection is part of the Blitz Project. It’s a progressive web app which aims is to help you do progressive enhancement.
+
+It is currently in v.0.10 (in other words, it’s a Minimum Viable Product) and available [on this page](https://friendsofepub.github.io/eBookTricks/).
+
+## Details
+
+This web app is just a glorified list of CSS snippets.
+
+- We’re using JS to toggle details.
+- We’re using Service Workers and appcache (fallback) to make it work offline.
+- Keyboard features are implemented via JS when needed.
+
+And that’s it. 
+
+## License 
+
+Blitz eBook Tricks, a Blitz tool to help you do progressive enhancement in eBooks
+
+Copyright (C) 2017–present Jiminy Panoz
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this program.  If not, see [http://www.gnu.org/licenses/](http://www.gnu.org/licenses/).
+
+### What LGPL3 actually implies
+
+You can fork this repo and make your own collection/web app. You can use it in commercial projets but your modifications to this specific part (consider this a library) should then be released with a LGPL3 license. Attribution is needed in any case.
+
+### Why LGPL3 OMG it’s not MIT like the Blitz Framework!
+
+The eBook dev ecosystem is a nightmare, we need eBook tools. And fast!
+
+Very few people actually release those tools. Now it’s just a checklist so why would you want to keep this closed source, huh?
---
+++ js/script.js
@@ -0,0 +1,101 @@
+r(function() {
+	// VARIABLES
+	
+	var details = document.getElementsByClassName('details'),
+			triggers = document.getElementsByClassName('summary');
+			main = document.getElementsByTagName('main')[0],
+			toggle = document.createElement('button'),
+			header = document.getElementsByTagName('header')[0];
+	
+	// FUNCTIONS
+	
+	function toggleAria(el) {
+		if (el.getAttribute('aria-hidden') === 'true') {
+			el.setAttribute('aria-hidden', 'false');
+		} else {
+			el.setAttribute('aria-hidden', 'true');
+		}
+	};
+	
+	function toggleDetails(trigger) {
+		var detail = trigger.nextElementSibling;
+		trigger.classList.toggle('open');
+		detail.classList.toggle('hidden');		
+		toggleAria(detail);
+	};
+
+	// INIT (run on doc Ready)
+		
+	(function hideDetails() {
+		document.getElementsByTagName('body')[0].classList.add('js-enabled');
+		for (var i = 0; i < details.length; i++) {
+			var detail = details[i];
+			detail.classList.add('hidden');
+			detail.setAttribute('aria-hidden', 'true');
+			detail.setAttribute('aria-live', 'polite');
+			detail.previousElementSibling.setAttribute('tabindex', '0');
+		};
+	})();
+	
+	(function initToggle() {
+	  toggle.type = 'button';
+	  toggle.id = 'toggle';
+	  toggle.className = 'checkAll';
+	  toggle.innerHTML = 'Expand all details';
+	  header.appendChild(toggle);
+	})();
+	
+	// Event Listeners 
+	
+	main.addEventListener('click', function(e) {
+		var elt = e.target;
+		var isSummary = elt.classList.contains('summary');
+		var isDetailsPara = elt.classList.contains('details-para');
+		
+		if (isSummary) {
+			e.preventDefault();
+			toggleDetails(elt);
+		} else if (isDetailsPara) {
+			e.preventDefault();
+		} else {
+			return;
+		}
+	});
+	
+	main.addEventListener('keyup', function(e) {
+	  var active = document.activeElement;
+		var isSummary = active.classList.contains('summary');
+		var pressEnter = (e.key === 'Enter' || e.keyCode === 13);
+		
+		if (isSummary && pressEnter) {
+			e.preventDefault();
+			toggleDetails(active);
+		} else {
+			return;
+		}
+	});
+	
+	toggle.addEventListener('click', function(e) {		  
+		e.preventDefault();
+		this.classList.toggle('toggleActive');
+		if (this.classList.contains('toggleActive')) {
+		  this.innerHTML = 'Collapse all details';
+		  for (var i = 0; i < details.length; i++) {
+		  	var detail = details[i];
+		    detail.classList.remove('hidden');
+		    detail.previousElementSibling.classList.add('open');
+		    detail.setAttribute('aria-hidden', 'false');
+		  };
+		} else {
+		  this.innerHTML = 'Expand all details';
+		  for (var i = 0; i < details.length; i++) {
+		  	var detail = details[i];
+		    detail.classList.add('hidden');
+		    detail.previousElementSibling.classList.remove('open');
+		    detail.setAttribute('aria-hidden', 'true');
+		  };
+		};
+	});
+
+});
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}																	
